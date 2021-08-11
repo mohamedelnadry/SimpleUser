c0a978cf888acb0048c6c93c7b57d4ee5874f8d9
mohamedelnadry <mohamdelnadry12345@gmail.com>
2021-08-11 04:21:07 +0200

    Simple User

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ app.use(require('./routes/Login'))
 app.get('/', (req, res) => {
     res.send('Home Page')
 })
-mongoose.connect('mongodb://localhost:27017/usersystem', {useNewUrlParser: true, useUnifiedTopology: true}).then((value)=>{
-    console.log("connected");
-}).catch((err)=>{
-    console.log("error in connection");
+// mongoose.connect('mongodb://localhost:27017/usersystem', {useNewUrlParser: true, useUnifiedTopology: true}).then((value)=>{
+//     console.log("connected");
+// }).catch((err)=>{
+//     console.log("error in connection");
+// });
+// app.listen(port, () => console.log(`Example app listening on port port!`))
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.fjtlc.mongodb.net/UserSystem', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('connected');
+}).catch(() => {
+  console.log('there are error in db');
 });
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port port!`))
